9e553b2f5c78ca86511a03f288bfa26c57bce03b
Eric Kim <48634064+hyeonukim@users.noreply.github.com>
2023-12-29 15:26:35 -0800

    init dodgeball

---
+++ Source/Dodgeball/EnemyCharacter.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EnemyCharacter.generated.h"
+
+UCLASS()
+class DODGEBALL_API AEnemyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+private: 
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = LookAt, meta = (AllowPrivateAccess = "true"))
+	class USceneComponent* SightSource;
+
+public:
+	// Sets default values for this character's properties
+	AEnemyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void LookAtActor(AActor* TargetActor);
+
+	bool CanSeeActor(const AActor* TargetActor) const;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
